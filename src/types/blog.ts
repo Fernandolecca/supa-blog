e78e34998d002377e1135d785f4515a45711d384
Fernando Lecca <fernandolecca93@gmail.com>
2023-03-03 00:21:04 -0500

    fix: correcting error with type Post making all props optional

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -1,1 +1,7 @@
-export type Post = { title: string; content: string; is_published: boolean };
+export type Post = {
+  title?: string;
+  content?: string;
+  is_published?: boolean;
+  post_id?: number;
+  created_by?: string;
+};
